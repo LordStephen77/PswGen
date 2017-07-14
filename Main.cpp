0ce4bbbc5758db0bd24956f35215b3434501bb84
LordStephen77 <lordstephen77@gmail.com>
2017-07-14 12:48:43 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,12 +4,12 @@ Copyright (C) 2017 Stefano Peris
 nickname: LordStephen77
 eMail: lordstephen77@gmail.com
 
-vanilla_decor is free software: you can redistribute it and/or modify it
+is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
 Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-vanilla_decor is distributed in the hope that it will be useful, but
+is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
